3f44c3a1f1e3fdd451e15cc0d43744fb1cfca215
th3mike <maarcio_lacerda@hotmail.com>
2021-05-17 23:19:21 -0300

    form validation

---
+++ src/components/contact.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="container mb-5">
+        <form @submit.prevent="sendEmail">
+          <label>Name</label>
+          <input 
+            type="text" 
+            v-model="name"
+            name="name"
+            placeholder="Your Name"
+          >
+          <label>Email</label>
+          <input 
+            type="email" 
+            v-model="email"
+            name="email"
+            placeholder="Your Email"
+            >
+          <label>Message</label>
+          <textarea 
+            name="message"
+            v-model="message"
+            cols="30" rows="5"
+            placeholder="Message">
+          </textarea>
+          
+          <input type="submit" value="Send">
+        </form>
+    </div>
+</template>
+
+<script>
+import emailjs from 'emailjs-com';
+export default {
+  name: 'ContactUs',
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    sendEmail(e) {
+      try {
+        emailjs.sendForm('hotmail', 'hotmail_template', e.target, 'user_vFEf5mRjRACvYJAFbNSsI', {
+          name: this.name,
+          email: this.email,
+          message: this.meessage
+        })
+        alert('it works!!!')
+      } catch (error) {
+          alert({error})
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
+    },
+  }
+}
+</script>
+
+<style scoped>
+* {box-sizing: border-box;}
+label {
+  float: left;
+}
+input[type=text], [type=email], textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+input[type=submit]:hover {
+  background-color: #b8c7b9;
+}
+.container {
+  display: block;
+  margin:auto;
+  text-align: center;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  width: 50%;
+}
+</style>
